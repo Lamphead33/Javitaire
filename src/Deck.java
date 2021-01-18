1c88cd09778fe14d88754be8ce08c7d7f999e58a
Lamphead33 <krysblaney@gmail.com>
2021-01-17 21:54:16 -0500

    Logic for deck initialization and shuffling.

---
+++ src/Deck.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> deck = new ArrayList<Card>();
+    private String[] suits = new String[]{"Hearts", "Diamonds", "Spades", "Clubs"};
+    
+    // Deck is populated upon creation
+    public Deck() {
+        // Generates the deck -
+        for (String currentSuit : suits) {
+            for (int i = 1; i < 14; i++ ) {
+                Card c = new Card(i, currentSuit);
+                deck.add(c);
+            }
+        }
+        // Shuffle the deck
+        Collections.shuffle(deck);
+    }
+    
+    
+    // Prints the deck (testing purposes)
+    public void printDeck() {
+        for (Card c: this.deck) {
+            System.out.println(c.getCard());
+        }
+        
+    }
+
+}
